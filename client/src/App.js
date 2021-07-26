7e607382dbbc5517364ea83e4dc4b6623cc8e165
Dirk Kinley <42229326+dkinley@users.noreply.github.com>
2021-07-26 08:41:45 -0700

    First round updates to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import GetAll from './components/GetAll';
+import Edit from './components/Edit';
+import Details from './components/Details';
+import { Router } from '@reach/router';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Router>
+        <GetAll path="/" />
+        <Edit path="/capacity/:id/edit" />
+        <Details path="/capacity/:id"/> 
+      </Router>
     </div>
   );
 }
-
 export default App;
