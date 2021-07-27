c1b856912e16cbf50aee0e531f8e4ce81ed6131f
Ross St. Clair <ross.stclair@gmail.com>
2021-07-27 09:45:36 -0400

    Capacity controllers and routes done

--- server/server.js
+++ server/server.js
@@ -4,9 +4,17 @@ const cors = require("cors");
 const app = express();
 require("./config/mongoose.config");
 
-app.use(cors());
+app.use(cors({
+	credentials: true,
+	origin: "http://localhost:3000",
+}));
+
+app.use(cookieParser());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//require("./routes/user.routes")(app);
+
+require("./routes/user.routes")(app);
+require("./routes/capacity.routes")(app);
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Server listening on port ${process.env.SERVER_PORT}`));
