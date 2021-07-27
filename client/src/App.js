d6b2119ab428ba849af259d379eef5e4d16ab5f6
Steele Smith <steelesmith@gmail.com>
2021-07-26 17:40:40 -0700

    styles added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
 import './App.css';
+import Login from './components/Login';
+import RegisterUser from './components/RegisterUser';
+import {Router} from '@reach/router';
 
 function App() {
+
+
   return (
     <div className="App">
+        <Router>
+              <Login default/>
+              <RegisterUser path="/signup"/>
+        </Router>
+          
     </div>
   );
 }
