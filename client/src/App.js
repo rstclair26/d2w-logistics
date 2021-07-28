87160656e6ee290f521b174be324b472141f5288
Dirk Kinley <42229326+dkinley@users.noreply.github.com>
2021-07-27 18:52:36 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import './App.css';
+import Login from './components/Login';
+import RegisterUser from './components/RegisterUser';
 import GetAll from './components/GetAll';
 import Create from './components/Create';
 import Edit from './components/Edit';
@@ -6,14 +9,18 @@ import Details from './components/Details';
 import { Router } from '@reach/router'; // since router is not exported as default {} are required
 
 function App() {
+
+
   return (
     <div className="App">
       <h1>Down To The Wire Logistics</h1>
       <Router>
-        <GetAll path="/" />
-        <Create path="/d2wLogisticsDBId/create" />
-        <Edit path="/d2wLogisticsDBId/:id/edit" />
-        <Details path="/d2wLogisticsDBId/:id"/> 
+        <Login default/>
+        <RegisterUser path="/signup"/>
+        <GetAll path="/capacities" />
+        <Create path="/capacities/create" />
+        <Edit path="/capacities/:id/edit" />
+        <Details path="/capacities/:id"/> 
       </Router>
     </div>
   );
