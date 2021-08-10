fc4a468fcfac2289eef36c54676732dad160b7c4
Ross St. Clair <81775714+rstclair26@users.noreply.github.com>
2021-08-10 18:39:43 -0400

    Delete d2w-logistics-main crud updates directory

--- d2w-logistics-main crud updates/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/" + process.env.DB_NAME, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Connection established to database"))
-    .catch(err => console.log("Error connecting to database", err))
